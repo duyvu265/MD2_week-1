7cf1fadecb0cd982515bbb7ce9f92db89212298c
thanh duy <you@example.com>
2023-02-09 16:07:03 +0700

    th3

---
+++ TH3.js
@@ -0,0 +1,17 @@
+//
+// function namelist(){
+//     return ["alpha","beta","gamma","delta"]
+// }
+// let [fistname,secondname]=namelist();
+// console.log(fistname,secondname)
+
+
+// let mark={x:21,y:-34,z:47};
+// const {x,y,z}=mark
+// console.log(x,y,z)
+const mark={
+    section1:{ alpha:15,beta:16},
+    section2:{ alpha: -31,beta: 19}
+}
+const {section1:{alpha:alpha1,beta:beta1}}=mark;
+console.log(alpha1,beta1)
