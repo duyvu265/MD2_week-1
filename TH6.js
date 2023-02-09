99112b3420080383938f8d86c1802a148594b053
thanh duy <you@example.com>
2023-02-09 20:38:07 +0700

    TH6

---
+++ TH6.js
@@ -0,0 +1,55 @@
+
+
+
+// function sum(x, y, z) {
+//     return x + y + z;
+// }
+//
+// const numbers = [1, 2, 3];
+// console.log(sum.apply(null, numbers));
+//
+//
+//
+//
+// function sum1(x, y, z) {
+//     return x + y + z;
+// }
+//
+// const numbers1 = [1, 2, 3];
+// console.log(sum1(...numbers1));
+//
+//
+//
+// let a1 = [1, 2];
+// a1.push([1, 2, 3]);
+// console.log(a1);
+//
+//
+//
+// let a1  = [1, 2];
+// a1.push.apply(a1,[1, 2, 3]);
+// console.log(a1);
+//
+//
+//
+// let a3 = [1, 2];
+// let b3 = [1,2,3];
+// a3.push(...b3);
+// console.log(a3);
+//
+//
+// let  a4 = [1, 2];
+// let b4= [1,2,3];
+// a4 = [...b4];
+// console.log(a4);
+//
+//
+// let obj1={a4:1, b4:2};
+// let obj2 = {...obj1};
+// console.log(obj2);
+//
+//
+// let a5 = [1,2];
+// let b5 = [3,4,5];
+// let c = [...a5,...b5];
+// console.log(c);
