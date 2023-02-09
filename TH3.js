fb2b5fa6003a47a56ba9810fc1b57f4f5ea4e3e4
thanh duy <you@example.com>
2023-02-09 16:40:43 +0700

    TH4

--- TH3.js
+++ TH3.js
@@ -9,9 +9,9 @@
 // let mark={x:21,y:-34,z:47};
 // const {x,y,z}=mark
 // console.log(x,y,z)
-const mark={
-    section1:{ alpha:15,beta:16},
-    section2:{ alpha: -31,beta: 19}
-}
-const {section1:{alpha:alpha1,beta:beta1}}=mark;
-console.log(alpha1,beta1)
+// const mark={
+//     section1:{ alpha:15,beta:16},
+//     section2:{ alpha: -31,beta: 19}
+// }
+// const {section1:{alpha:alpha1,beta:beta1}}=mark;
+// console.log(alpha1,beta1)
